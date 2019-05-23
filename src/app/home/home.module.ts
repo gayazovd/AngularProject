ffba12a8b6542fc3bd0536db186ad206e5a02a03
Denis Gaiazov <Denis_Gaiazov@epam.com>
2019-05-23 19:22:03 +0400

    add courses list component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesNavigationComponent } from './courses-navigation/courses-navigation.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesListItemComponent } from './courses-list-item/courses-list-item.component';
 
 @NgModule({
-    declarations: [CoursesNavigationComponent],
+    declarations: [CoursesNavigationComponent, CoursesListComponent, CoursesListItemComponent],
     imports: [CommonModule],
-    exports: [CoursesNavigationComponent]
+    exports: [CoursesNavigationComponent, CoursesListComponent, CoursesListItemComponent]
 })
 export class HomeModule {}
