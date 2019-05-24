8d1544fd086275b45d188db7d8040507583c0cb9
Denis Gaiazov <Denis_Gaiazov@epam.com>
2019-05-24 19:03:05 +0400

    add courses list and courses navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Courses-angular';
+    public coursesItems: any[] = [
+        { id: 1, itemsQuantity: 3 },
+        { id: 2, itemsQuantity: 1 },
+        { id: 3, itemsQuantity: 2 },
+        { id: 4, itemsQuantity: 4 }
+    ];
+    title = 'Courses-angular';
 }
